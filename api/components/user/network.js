cf27912d9a48d87d862058491fbde6e26d283eb8
EiffelIKE <jctrujillo759@gmail.com>
2022-12-09 18:50:27 -0800

    Was installed dotenv dependency to manage the local env variables. Main operations Post and gets created,was installed nanoid for auto generate users id if not provided on the req.body, and added body-parser dependency to parse all requests body into JSON, reorganized the user/controller.js file by route.CRUD and the internal functions

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -5,9 +5,43 @@ const controller = require('./index')
 
 const router = express.Router()
 
-router.get('/', function (req, res){
-  const list = controller.list()
-  response.success(req, res, list, 200)
-})
+// CRUD Routes
+router.get('/', list)
+router.get('/:id', get)
+router.post('/', upsert)
+router.put('/', upsert)
+
+
+//Internal fx()
+function list (req, res){
+  controller.list()
+    .then((list) => {
+      response.success(req, res, list, 200)
+    })
+    .catch( err => {
+      response.error(req, res, err.message, 500)
+    })
+  
+}
+
+function get (req, res){
+  controller.get(req.params.id)
+    .then((user) => {
+      response.success(req, res, user, 200)
+    })
+    .catch( err => {
+      response.error(req, res, err.message, 500)
+    })  
+}
+
+function upsert (req, res){
+  controller.upsert(req.body)
+    .then( user =>{
+      response.success(req, res, user, 201)
+    })
+    .catch( err => {
+      response.error(req, res, err.message, 500)
+    })
+}
 
 module.exports = router
--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const config = require('../config')
 const user = require('./components/user/network')
 
 const app = express()
 
+app.use(bodyParser.json())
 //Router
 app.use('/api/user', user)
 
 app.listen(config.api.port, () => {
-  console.log('API running on port ', config.api.port)
+  console.log('[API] Running on port ',config.api.port)
 })
